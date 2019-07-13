ab098df873290ab6c951ee1adb26825eec11f02d
Suprianto D <ersaka96@gmail.com>
2019-07-13 12:38:52 +0800

    Initial Commit

---
+++ Instagram Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram Clone
+//
+//  Created by Suprianto Djamalu on 13/07/19.
+//  Copyright © 2019 Suprianto Djamalu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
